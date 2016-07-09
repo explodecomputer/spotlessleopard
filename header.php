a28b6f96294de918099d59975826212672d59f7a
Gibran Hemani <explodecomputer@gmail.com>
2016-07-09 05:53:18 -0400

    added ga script

--- header.php
+++ header.php
@@ -36,6 +36,20 @@
 
 <?php wp_head();?>
 
+<!-- google analytics -->
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-64755036-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
 </head>
 
 <?php write_log('Visit: ' . $wp_query->post->post_title); ?>
